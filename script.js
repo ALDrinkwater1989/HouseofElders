54788cbdeb65efe5987d9552f9a70ef87ed0a163
Alex <aldrinkwater@hotmail.com>
2019-07-18 16:43:26 +0100

    new home page

--- script.js
+++ script.js
@@ -1,25 +1,16 @@
-const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelectorAll('.nav-links li');
+var apiKey = "f5bc87c4755348eea655dadb9b86c125";
+
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "https://www.bungie.net/platform/Destiny/Manifest/InventoryItem/1274330687/", true);
+xhr.setRequestHeader("X-API-Key", apiKey);
 
 
-    burger.addEventListener('click', () => {
-        //Toggle nav
-        nav.classList.toggle('nav-active');
-
-        //Animate Links
-        navLinks.forEach((link, index) => {
-            if (link.style.animation) {
-                link.style.animation = '';
-            } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
-            }
-        });
-
-        //burger Animation
-        burger.classList.toggle('toggle-ani');
-    });
-}
-
-navSlide();
+xhr.onreadystatechange = function(){
+ if(this.readyState === 4 && this.status === 200){
+    var json = JSON.parse(this.responseText);
+    console.log(json.Response.data.inventoryItem.itemName); //Gjallarhorn
+    
+   }
+  }
+  
+  xhr.send();
